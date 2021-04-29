093e412edaa5da14ed51b2641e2dfca539c3d03d
Li-HONGYAO <lihy_online@163.com>
2021-04-29 17:46:36 +0800

    内容更新

--- 示例代码/01.IndexDB/index.js
+++ 示例代码/01.IndexDB/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Li-HONGYAO
  * @Date: 2021-04-19 17:00:50
- * @LastEditTime: 2021-04-19 18:32:10
+ * @LastEditTime: 2021-04-20 10:18:23
  * @LastEditors: Li-HONGYAO
  * @Description:
  * @FilePath: \01.IndexDB\index.js
@@ -11,13 +11,55 @@
   // 1. 打开数据库
   const db = await openDB("TEST-DATABASE");
   // addUsr();
+  // getUsr();
+  // readAll();
+  update();
+  function update() {
+    const objectStore = db.transaction(["USRS"], "readwrite").objectStore("USRS")
+    const request = objectStore.get(1);
+    request.onsuccess = e => {
+      const data = e.target.result;
+      data.phone = '17398888669';
+      const putRequest = objectStore.put(data);
+      putRequest.onsuccess = e => {
+        console.log('Put success!')
+      }
+    }
+  }
+  function readAll() {
+    const objectStore = db.transaction("USRS").objectStore("USRS");
+    const res = [];
+    objectStore.openCursor().onsuccess = function (event) {
+      var cursor = event.target.result;
+      if (cursor) {
+        res.push(cursor.value);
+        cursor.continue();
+      } else {
+        console.log(res);
+        console.log("No more entries!");
+      }
+    };
+  }
+  function getUsr() {
+    const request = db.transaction(["USRS"]).objectStore("USRS").get(3);
+    request.onsuccess = (e) => {
+      console.log(request.result);
+    };
+    request.onerror = (e) => {
+      console.log("Get fail with:", e);
+    };
+  }
   function addUsr() {
-    const request = db.transaction(["USRS"], "readwrite").objectStore('USRS').add({
-      name: "李鸿耀",
-      age: 28,
-      gender: "男",
-      address: "成都市高新区亚和南四路216号",
-    });
+    const request = db
+      .transaction(["USRS"], "readwrite")
+      .objectStore("USRS")
+      .add({
+        name: "苟玉梅",
+        age: 22,
+        gender: "女",
+        address: "成都市高新区亚和南四路216号",
+        phone: "18080606214",
+      });
     request.onsuccess = (e) => {
       console.log("Add success!");
     };
