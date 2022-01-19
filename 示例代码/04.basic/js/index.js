af33525828eb9b9c78d0a77e1f4b86ac608b25c2
Li-HONGYAO <lihy_online@163.com>
2022-01-19 18:12:09 +0800

    updateCodes

--- 示例代码/04.basic/js/index.js
+++ 示例代码/04.basic/js/index.js
@@ -1,16 +1,5 @@
-/*
- * @Author: Lee
- * @Date: 2022-01-17 15:28:13
- * @LastEditors: Lee
- * @LastEditTime: 2022-01-18 17:55:28
- */
-
-var obj = {
-  id: 'awesome',
-  cool: function coolFn() {
-    console.log(this.id);
-  },
-};
-var id = 'not awesome';
-obj.cool(); // awesome
-setTimeout(obj.cool, 100); // not awesome
+var o = { a: 1 };
+function t() {
+  console.log(this)
+}
+[1, 2, 3].forEach(t, o);
